39bf8feb1cfb2bad8f40ffe47d6879e2e6741576
Kirk Bushell <torm3nt@gmail.com>
2023-11-26 22:52:04 +1100

    Simplified the code considerably.

--- src/Actions/DownAction.php
+++ src/Actions/DownAction.php
@@ -3,26 +3,19 @@
 namespace Antwerpes\FilamentNestedSetOrder\Actions;
 
 use Filament\Tables\Actions\Action;
-use Illuminate\Support\Facades\Cache;
 
-class Down extends Action
+class DownAction extends Action
 {
     protected function setUp(): void
     {
         parent::setUp();
 
         $this
-            ->label(__('down'))
             ->icon('heroicon-s-arrow-circle-up')
             ->iconButton()
             ->action(static fn ($record) => $record->moveOrderUp())
             ->visible(static function ($record) {
-                $last = config('filament-nested-set-order.cache_enabled')
-                    ? Cache::rememberForever(
-                        $record->getOrderableCachePrefix().'-last',
-                        static fn () => $record->buildSortQuery()->ordered()->last(),
-                    )
-                    : $record->buildSortQuery()->ordered()->last();
+                $last = $record->buildSortQuery()->ordered()->last();
 
                 return $record->id !== $last?->id;
             });
